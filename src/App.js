624f08fd62fc6140c0f9e74fe9370a104ad3a190
serignediaw <smdiaw@ucdavis.edu>
2023-12-09 15:06:37 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 
 import React from "react";
 import About from "./components/About";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">     
-      <Navbar />
+    <main className="text-gray-400 bg-gray-900 body-font">  
+      <Navbar />   
       <About />
       <Projects />
       <Skills />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+// src/components/Navbar.js
+
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+    return(
+        <div>navbar</div>
+    )
+}
