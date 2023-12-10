2b903b17815874c33a94691c1556644a84207141
serignediaw <smdiaw@ucdavis.edu>
2023-12-09 19:27:40 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">  
+    <div className="App">  
       <Navbar />   
       <About />
       <Projects />
       <Skills />
-    </main>
+    </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,17 @@
 
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 export default function Navbar() {
     return(
-        <div>navbar</div>
+        <nav className="navBar">
+            <img src={logo} width={347} height={250} alt="Logo" className="logo" />
+            <div className="desktopMenu">
+
+            </div>
+            <button className="desktopMenuBtn">
+                <img src="" alt="" className="desktopMenuImg" />Contact Me</button>
+        </nav>
     )
 }
